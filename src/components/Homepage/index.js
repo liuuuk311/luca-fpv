b65ba6625b82a6405370a9b66b68a07b191310cc
Luca Palonca <luca.palonca@sailogy.com>
2020-03-21 20:59:12 +0100

    added new component

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,3 +1,4 @@
 export { default as HomeHero } from './HomeHero';
 export { default as HomePostList } from './HomePostList';
 export { default as HomeNewsletter } from './HomeNewsletter';
+export { default as HomeTopCategories } from './HomeTopCategories';
