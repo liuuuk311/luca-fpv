277f60f734fa25f687a49ddb04c2714e4bbd680f
Luca Palonca <luca.palonca@sailogy.com>
2020-09-16 23:38:05 +0200

    fixed gtm for compiling properly

--- src/components/Layout/Default/index.js
+++ src/components/Layout/Default/index.js
@@ -13,13 +13,6 @@ const Default = props => {
         
         <div>
             <Helmet>
-                <script type="text/javascript">{`
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-KR695CJ'); 
-                `}</script>
                 <html lang="it" />
                 <meta charset="UTF-8" />
                 <meta property="og:title" content="Luca FPV"/>
@@ -28,11 +21,8 @@ const Default = props => {
                 <meta property="og:description" content="Articoli utili su Droni, Quadricotteri, Mini Quad, Elettronica e Software. Guide, tutorial e recensioni sul mondo FPV. Il punto di riferimento per i piloti Itaiani FPV."/>
                 {/* <meta property="og:image" content="http://lucafpv.com/avatar.png"/> */}
                 <meta property="og:url" content={url}/>
+                
             </Helmet>
-
-            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KR695CJ"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-
             <Navigation/>
             <div>
             {children}
