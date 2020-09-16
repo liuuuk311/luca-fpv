2340b99f968c67aeff5fab2efc1e497426763ee9
Luca Palonca <luca.palonca@sailogy.com>
2020-09-16 23:03:43 +0200

    added gtm scripts for external link tracking

--- src/components/Layout/Default/index.js
+++ src/components/Layout/Default/index.js
@@ -10,8 +10,16 @@ const Default = props => {
     const url = location.href ? location.href : '';
 
     return (
+        
         <div>
             <Helmet>
+                <script type="text/javascript">{`
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-KR695CJ'); 
+                `}</script>
                 <html lang="it" />
                 <meta charset="UTF-8" />
                 <meta property="og:title" content="Luca FPV"/>
@@ -21,6 +29,10 @@ const Default = props => {
                 {/* <meta property="og:image" content="http://lucafpv.com/avatar.png"/> */}
                 <meta property="og:url" content={url}/>
             </Helmet>
+
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KR695CJ"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
             <Navigation/>
             <div>
             {children}
