92d158efab07c6140fd8112bd062ab6491658313
Luca Palonca <luca.palonca@sailogy.com>
2021-08-26 11:55:17 +0200

    meta description

--- src/pages/problemi-comuni-droni-fpv/index.js
+++ src/pages/problemi-comuni-droni-fpv/index.js
@@ -20,7 +20,7 @@ const Landing = ({ location }) => {
         >
             <Helmet>
                 <title>Problemi comuni con Droni FPV</title>
-                <meta name="description" content="Contatta Luca"/>
+                <meta name="description" content="Ecco la prima guida in italiano per risolvere i problemi più comuni con i droni FPV. Come ad esempio, il drone non si arma o vola male, il segnale video non si vede bene. "/>
             </Helmet>
             
                 <ValueProp/>
