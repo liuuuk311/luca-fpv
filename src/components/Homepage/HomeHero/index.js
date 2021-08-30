b4ad8947767a0b4d53853805a895f23fccb7fa78
Luca Palonca <luca.palonca@sailogy.com>
2021-08-30 21:10:51 +0200

    fix

--- src/components/Homepage/HomeHero/index.js
+++ src/components/Homepage/HomeHero/index.js
@@ -22,6 +22,24 @@ const HomeHero = () => {
             </Paragraph>
             <SubHeading>Cerchi qualcosa di specifico?</SubHeading>
             <div id="search"></div>
+            <script type="text/javascript" dangerouslySetInnerHTML={{
+                __html: `
+                    algoliasearchNetlify({
+                    appId: 'G0M23X5AHA',
+                    apiKey: 'f64ebf0353bcad837657a655e4523786',
+                    siteId: 'c3f13e8c-97b2-4e0f-b137-9df20f9f0153',
+                    branch: 'master',
+                    selector: '#search',
+                    placeholder: 'Cerca...',
+                    theme: {
+                        mark: '#eda550',                                 // Color of the matching content
+                        background: '#fff',                        // Background Color of the input and the panel
+                        selected: '#f5f5fa',                          // Background Color of the selected item
+                        text: '#23263b',                              // Color of the title of the items
+                        colorSourceIcon: '#d6d6e7'                    // Color of the icon on the left of results
+                    }
+                });
+                ` }} />
         </Container>
     );
 }
