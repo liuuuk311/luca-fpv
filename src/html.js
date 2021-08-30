696e124d9fe13f5fc0dcd5f2da0fec006649d3ef
Luca Palonca <luca.palonca@sailogy.com>
2021-08-30 20:29:05 +0200

    fix

--- src/html.js
+++ src/html.js
@@ -27,16 +27,6 @@ export default function HTML(props) {
           dangerouslySetInnerHTML={{ __html: props.body }}
         />
         {props.postBodyComponents}
-        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.js"></script>
-        <script type="text/javascript" dangerouslySetInnerHTML={{
-          __html: `
-          algoliasearchNetlify({
-            appId: 'G0M23X5AHA',
-            apiKey: 'f64ebf0353bcad837657a655e4523786',
-            siteId: 'c3f13e8c-97b2-4e0f-b137-9df20f9f0153',
-            branch: 'master',
-            selector: 'div#search',
-          });` }} />
       </body>
     </html>
   )
