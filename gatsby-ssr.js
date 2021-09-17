1e0881b0f3a57730ab7c151047df6efb90a39a45
Luca Palonca <luca.palonca@sailogy.com>
2021-09-17 15:22:57 +0200

    try to speed loading

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,9 +1,12 @@
-const React = require('react')
+import React from 'react';
+import Helmet from 'react-helmet';
 
-const HeadComponents = [
-  <script async data-ad-client="ca-pub-4566556883137005" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"/>,
-]
-
-exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
-  setHeadComponents(HeadComponents)
-}
+export const onClientEntry = () => <Helmet> 
+  <link rel="preconnect" href="https://adservice.google.com/"/>
+<link rel="preconnect" href="https://googleads.g.doubleclick.net/"/>
+<link rel="preconnect" href="https://www.googletagservices.com/"/>
+<link rel="preconnect" href="https://tpc.googlesyndication.com/"/>
+<link rel="preload" href="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" as="script"/>
+  <script async
+    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" data-ad-client="ca-pub-4566556883137005"
+  /></Helmet>;
