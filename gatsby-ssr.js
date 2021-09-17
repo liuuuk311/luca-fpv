0d355204d523666dae431b0726d9108fad0da0ad
Luca Palonca <luca.palonca@sailogy.com>
2021-09-17 15:40:52 +0200

    trying to fix

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,7 +6,7 @@ const HeadComponents = [
   <link rel="preconnect" href="https://www.googletagservices.com/"/>,
   <link rel="preconnect" href="https://tpc.googlesyndication.com/"/>,
   <link rel="preconnect" href="https://pagead2.googlesyndication.com/"/>,
-  <link rel="preload" href="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" as="script"/>,,
+
   <script async data-ad-client="ca-pub-4566556883137005" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"/>,
 ]
 
