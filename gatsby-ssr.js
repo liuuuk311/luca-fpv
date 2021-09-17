7a56a12e008c668bbc44e48b0da0abdd592b554e
Luca Palonca <luca.palonca@sailogy.com>
2021-09-17 15:32:51 +0200

    trying to fix

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import Helmet from 'react-helmet';
+const React = require('react')
 
-export const onClientEntry = () => <Helmet> 
-  <link rel="preconnect" href="https://adservice.google.com/"/>
-<link rel="preconnect" href="https://googleads.g.doubleclick.net/"/>
-<link rel="preconnect" href="https://www.googletagservices.com/"/>
-<link rel="preconnect" href="https://tpc.googlesyndication.com/"/>
-<link rel="preload" href="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" as="script"/>
-  <script async
-    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" data-ad-client="ca-pub-4566556883137005"
-  /></Helmet>;
+const HeadComponents = [
+  <link rel="preconnect" href="https://adservice.google.com/"/>,
+  <link rel="preconnect" href="https://googleads.g.doubleclick.net/"/>,
+  <link rel="preconnect" href="https://www.googletagservices.com/"/>,
+  <link rel="preconnect" href="https://tpc.googlesyndication.com/"/>,
+  <link rel="preconnect" href="https://pagead2.googlesyndication.com/"/>,
+  <link rel="preload" href="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" as="script"/>,,
+  <script async data-ad-client="ca-pub-4566556883137005" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"/>,
+]
+
+exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
+  setHeadComponents(HeadComponents)
+}
+
