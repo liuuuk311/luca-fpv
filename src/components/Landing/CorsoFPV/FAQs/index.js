7dde8c78e1ec949a069312f6c3bf4040138b82e2
luca <lucapalonca@gmail.com>
2022-08-04 22:31:08 +0200

    modifiche topina

--- src/components/Landing/CorsoFPV/FAQs/index.js
+++ src/components/Landing/CorsoFPV/FAQs/index.js
@@ -16,12 +16,12 @@ const FrequentlyAskedQuestions = () => {
         <Section>
             <Container>
                 <Heading>Quando si fanno le lezioni?</Heading>
-                <Body>Le lezioni teoriche online si tengono la sera in settimana. Tipicamente durano al massimo un paio di ore. Per quanto riguarda gli esercizi sul simulatore, l'allievo avrà la possibilità di esercitarsi in autonomia nel suo tempo libero. Infine, le lezioni pratiche tipicamente si tengono il sabato dalla mattina al pomeriggio. Se desideri avere un calendario dettagliato, <a href="#iscriviti">contattaci</a>!</Body>
+                <Body>Le lezioni teoriche online si tengono la sera in settimana e tipicamente durano un paio di ore al massimo. Per quanto riguarda gli esercizi sul simulatore, l'allievo avrà la possibilità di esercitarsi in autonomia nel tempo libero. Infine, le lezioni pratiche si svolgono il sabato dalla mattina al pomeriggio. Se desideri ricevere un calendario più dettagliato, <a href="#iscriviti">contattaci</a>!</Body>
                 <Heading>Di cosa ho bisogno per partecipare?</Heading>
-                <Body><strong>L'unica cosa di cui hai bisogno è un computer e almeno il patentino A1/A3</strong>. Il computer ti servirà per poter partecipare alle lezioni online e ad esercitarti sul simulatore. <strong>Se ti manca il patentino, non ti preoccupare. Potrai farlo durante il corso.</strong> Il noleggio del Radiocomando, Occhiali, Drone FPV, e una licenza del simulatore, è incluso.</Body>
+                <Body>L'unica cosa di cui hai bisogno per partecipare al corso è un computer per seguire le lezioni online ed esercitarti sul simulatore. Sono invece inclusi nel corso, il noleggio del radiocomando, occhiali, drone FPV e una licenza del simulatore.</Body>
                 <Heading>È possibile fare lezioni extra?</Heading>
-                <Body>Si, è possibile richiedere lezioni extra per approfondire diversi aspetti tra cui: Come costruire un drone FPV, approfondimento sul PID Tuning, e infine tutto il necessario per lavorare con droni FPV. </Body>
-                <Body>È anche possibile richiedere lezioni individuali e consulenze per rispondere a tutti i vostri dubbi.</Body>
+                <Body>Sì, è possibile richiedere lezioni extra per approfondire diversi aspetti tra cui: costruzione di un drone FPV, PID Tuning e infine, tutto il necessario per lavorare con droni FPV. </Body>
+                <Body>Sono disponibili su richiesta anche lezioni individuali e consulenze personalizzate per rispondere a eventuali dubbi specifici.</Body>
             </Container>
         </Section>
     )
--- src/components/Landing/CorsoFPV/ProductBenefits/index.js
+++ src/components/Landing/CorsoFPV/ProductBenefits/index.js
@@ -19,26 +19,25 @@ const ProductBenefit = () => {
             <ProductBenefitGroup>
                 <TagLine1>
                     <h2>4 lezioni di teoria online</h2>
-                    <p>Per garantire la massima flessibilità agli allivi, per questa seconda edizione del corso, abbiamo deciso di tenere le lezioni pratiche online.</p>
-                    <p>Non sarà il classico video corso, le lezioni saranno effettuate in videochiamata, in modo tale che ci possa essere interazioni tra allivi e istruttori, inoltre in questo modo è possibile fare domande e ottenere risposte immediatamente.</p>
+                    <p>Per garantire la massima flessibilità agli allievi, per questa seconda edizione del corso, abbiamo deciso di tenere le lezioni teoriche online.</p>
+                    <p>Non sarà il classico video corso. Le lezioni saranno effettuate in videochiamata, in modo tale da garantire le interazioni tra allievi e istruttori e permettere di fare domande e ottenere risposte immediatamente.</p>
                 </TagLine1>
                 <Image1/>
                 <TagLine2>
                     <h2>7 esercizi guidati con il simulatore</h2>
                     <p>Prima di pilotare un vero drone FPV è fondamentale esercitarsi in un ambiente sicuro, dove gli errori non costano caro e non sono pericolosi.</p>
-                    <p>Per questo motivo abbiamo sviluppato degli esercizi che verranno svolti sul simulatore, che permettono di consolidare le basi del volo in modalità acro. </p>
+                    <p>Per questo motivo abbiamo sviluppato degli esercizi guidati che verranno svolti sul simulatore Velocidrone, che permettono di consolidare le basi del volo in modalità acro. </p>
                 </TagLine2>
                 <Image2/>
                 <TagLine3>
                     <h2>2 lezioni pratiche</h2>
-                    <p>Sono previste due giornate alla scuola di volo, in cui gli allievi, supervisionati dagli istruttori, potranno volare con i droni forniti dalla base.</p>
-                    <p>In queste sessioni gli allievi, condurranno le manovre imparate sul simulatore per poi concludere il corso con un esame pratico.</p>
+                    <p>Sono previste due giornate alla base Italdron di Annone Brianza (LC), in cui gli allievi, supervisionati dagli istruttori, potranno volare con i droni forniti dalla scuola di volo.</p>
+                    <p>In queste due sessioni, gli allievi avranno la possibilità di effettuare dal vivo le manovre imparate sul simulatore per poi concludere il corso con un esame pratico.</p>
                 </TagLine3>
                 <Image3/>
                 <TagLine4>
                     <h2>Supporto costante</h2>
-                    <p>Gli istruttori saranno a tua completa disposizione durante l'intero corso, sia in presenza che tramite piattaforme Web-Chat.</p>
-                    <p>Per questo motivo abbiamo sviluppato degli esercizi che verranno svolti sul simulatore, che permettono di consolidare le basi del volo in modalità acro. </p>
+                    <p>Durante tutta la durata del corso, gli istruttori saranno a completa disposizione degli allievi, sia in presenza che tramite app di messaggistica per rispondere a qualsiasi dubbio e perplessità.</p>
                 </TagLine4>
                 <Image4/>
             </ProductBenefitGroup>
