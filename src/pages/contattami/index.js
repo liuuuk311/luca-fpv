294299899c4beeb287da61dcc9248ecf4f7a8455
luca <lucapalonca@gmail.com>
2022-08-11 15:58:51 +0200

    refactor

--- src/pages/contattami/index.js
+++ src/pages/contattami/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+import { EmailField, MessageArea, NameField } from '../../components/inputs';
 
 
 const Contact = () => {
@@ -25,19 +26,9 @@ const Contact = () => {
                     </p>
                 </div>
                 <form action="https://getform.io/f/16416a86-7a7c-4646-817a-3e0c45a40ae5" method="POST">
-                    <div className="mb-4">
-                        <label htmlFor="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Nome</label>
-                        <input type="text" id="name" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Mario Rossi" />
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
-                        <input type="email" id="email" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="nome.cognome@email.com" />
-                        <p id="helper-text-explanation" class="mt-2 text-sm text-gray-500 dark:text-gray-400">Il tuo indirizzo email non verrà condiviso con terze parti, e <strong>non riceverai mai spam</strong>.</p>
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Messaggio</label>
-                        <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ciao Luca, ti scrivo perché..." />
-                    </div>
+                    <NameField id="name" name="name"/>
+                    <EmailField id="email" name="email"/>
+                    <MessageArea id="message" name="message"/>
                     <div>
                         <button
                             type="submit"
